63209864f97d6dc77d8396c09fcfa54baf59021a
hmilner <hmilner@conviva.com>
2019-05-27 10:52:30 -0700

    [PR-39] Working version of memoized length computation.

--- src/test/scala/pbdirect/PBWriterBenchmark.scala
+++ src/test/scala/pbdirect/PBWriterBenchmark.scala
@@ -7,26 +7,20 @@ object PBWriterBenchmark extends Bench.LocalTime {
   private def make5DeepTuple[V](rootValue: V): Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[V]]]]] = {
     Tuple1(Tuple1(Tuple1(Tuple1(Tuple1(rootValue)))))
   }
-
   private def make10DeepTuple[V](rootValue: V): Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[V]]]]]]]]]] = {
     make5DeepTuple(make5DeepTuple(rootValue))
   }
-
   private def make20DeepTuple[V](rootValue: V): Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[V]]]]]]]]]]]]]]]]]]]] = {
     make10DeepTuple(make10DeepTuple(rootValue))
   }
 
-  private def make40DeepTuple[V](rootValue: V): Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[Tuple1[V]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] = {
-    make20DeepTuple(make20DeepTuple(rootValue))
-  }
-
   private val sizes = Gen.range("size")(100000, 1000000, 100000)
-
-  private val fiveDeep = for (size <- sizes) yield make5DeepTuple(Array.fill(size)(1.toByte))
-  private val tenDeep = for (size <- sizes) yield make10DeepTuple(Array.fill(size)(1.toByte))
-  private val twentyDeep = for (size <- sizes) yield make20DeepTuple(Array.fill(size)(1.toByte))
-  private val fortyDeep = for (size <- sizes) yield make40DeepTuple(Array.fill(size)(1.toByte))
-
+  private def makePayload(size: Int) = Array.fill(size)(1.toByte)
+  
+  private val fiveDeep = for (size <- sizes) yield make5DeepTuple(makePayload(size))
+  private val tenDeep = for (size <- sizes) yield make10DeepTuple(makePayload(size))
+  private val twentyDeep = for (size <- sizes) yield make20DeepTuple(makePayload(size))
+  
   performance of "5-Deep Tuple" in {
     measure method "toPB" in {
       using(fiveDeep) in {
@@ -46,14 +40,6 @@ object PBWriterBenchmark extends Bench.LocalTime {
   performance of "20-Deep Tuple" in {
     measure method "toPB" in {
       using(twentyDeep) in {
-        d => d.toPB
-      }
-    }
-  }
-
-  performance of "40-Deep Tuple" in {
-    measure method "toPB" in {
-      using(fortyDeep) in {
         d => d.toPB
       }
     }
